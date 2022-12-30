4e846f7421e3eaf15dfda65b8b306775f3e893af
Prem041199 <premkumarmettur@gmail.com>
2022-12-30 13:23:42 +0530

    Message

--- src/test/java/org/kumar/Laptop.java
+++ src/test/java/org/kumar/Laptop.java
@@ -17,19 +17,6 @@ public class Laptop {
 		System.out.println("No 1");
 
 	}
-<<<<<<< HEAD
-	private void tamil() {
-		System.out.println("Tamilnadu speaks tamil");
-
-	}
-	private void english() {
-		System.out.println("All the country speaks English");
-	}
-	private void telugu() {
-		System.out.println("Telugu speaks in andhra and Telugana");
-	}
-	
-=======
 	private void wipro() {
 		System.out.println("Wipro is an level 1 comapny");
 	}
@@ -41,6 +28,17 @@ public class Laptop {
 		System.out.println("It is Level 2");
 
 	}
->>>>>>> a2af2557a23a58fd1ad38a4af30bdce9c7e77016
+	
+	private void tamil() {
+		System.out.println("Tamilnadu speaks tamil");
+
+	}
+	private void english() {
+		System.out.println("All the country speaks English");
+	}
+	private void telugu() {
+		System.out.println("Telugu speaks in andhra and Telugana");
+	}
+	
 
 }
