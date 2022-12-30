bcddfb52a3ec916e5f9683db42b87f9c3b924091
Prem041199 <premkumarmettur@gmail.com>
2022-12-30 12:23:46 +0530

    new code

--- src/test/java/org/kumar/Laptop.java
+++ src/test/java/org/kumar/Laptop.java
@@ -1,5 +1,22 @@
 package org.kumar;
 
 public class Laptop {
+	private void Hp() {
+		System.out.println("No 2 ");
+
+	}
+	private void Dell() {
+		System.out.println("No 3");
+
+	}
+	private void Lenova() {
+		System.out.println("No 4");
+
+	}
+	private void Apple() {
+		System.out.println("No 1");
+
+	}
+	
 
 }
